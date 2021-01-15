de07fa803e9850d8655891eaeb8feeaedfee2317
Slickroot <elaich@gmx.net>
2021-01-15 13:23:33 +0100

    I loved this pipeline, it sounds easy enough

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Marouane!'));
+app.get('/', (req, res) => res.send('<h2>Hello Marouane!</h2><h4>Unbelivably easy to push and deploy</h4>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
